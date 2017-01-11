85e7d107c4827e7ef6b5de821ed1745ea92d6611
git-mormot <lovemormot@gmail.com>
2017-01-10 20:06:38 -0500

    added i18n

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,20 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    resources :products do
+      get :who_bought, on: :member
+    end
+    root to: 'store#index', as: 'store'
+  end
+  
   get 'store/index'
 
-  resources :products do
-  	get :who_bought, on: :member
-  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'store#index', as: 'store'
+  
 end
